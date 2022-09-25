21632506b00448abde4d3449e5baf3e7566e3a70
Kenneth Solomon <synecders@gmail.com>
2022-09-26 00:14:20 +0800

    count room

--- cloud/functions.js
+++ cloud/functions.js
@@ -3,6 +3,32 @@ Parse.Cloud.define('hello', req => {
   return 'Hi';
 });
 
+Parse.Cloud.define('countRoom', async (request) => {
+    const query = new Parse.Query('boarder')
+    // Use Session Token to Authenticate User
+    // const result = await query.find({sessionToken: request.user.getSessionToken()})
+
+    const pipeline = [
+        {
+          '$group': {
+            '_id': '$_p_roomPointer',
+            'count': {
+              '$count': {}
+            }
+          }
+        }
+      ];
+
+      return query.aggregate(pipeline)
+        .then(function (results) {
+          return results;
+        })
+        .catch(function (error) {
+          throw error;
+        });
+});
+
+
 Parse.Cloud.define('users', req => {
   const query = new Parse.Query(Parse.User);
   // query.equalTo("objectId", "lltcILRljN")
